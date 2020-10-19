e5b712ffa136d982071d7292ab3f19714d5fced0
poojan <savanipoojan78@gmail.com>
2020-10-19 11:05:44 +0530

    Observable added

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { auth } from 'firebase/app';
 import * as firebase from 'firebase'
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent{
-  user: firebase.User;
+  user$: Observable<firebase.User>;
   
-  constructor(private auth: AngularFireAuth) {
-    auth.authState.subscribe(user=>this.user=user);
+  constructor(private auth: AngularFireAuth) {  
+    this.user$=auth.authState;
   }
 
   logout() {
